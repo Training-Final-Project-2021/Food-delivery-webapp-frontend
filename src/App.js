afa3be8bd59b22e93fd396bce4ccd94fc8bc7386
uniyalvipin <uniyalvipin70@gmail.com>
2021-07-12 11:27:36 +0530

    initial react setup and added some files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import Main from './components/Main';
+import {BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      React APP
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Main />
+      </div>
+    </BrowserRouter>
   );
 }
 
